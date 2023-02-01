ab37eb52974e86c25be848d53b15e5c908d8464e
Iulia <iulia.puiu@hotmail.com>
2023-02-01 14:16:19 +0200

    Refactor store with RTK query

--- src/services/DataServices/api.ts
+++ src/services/DataServices/api.ts
@@ -1,1 +1,22 @@
-export const baseUrl = 'https://0e2d-86-126-141-188.ngrok.io/api/'
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query'
+
+export const apiSlice = createApi({
+    reducerPath: 'api',
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://ec2-3-93-58-72.compute-1.amazonaws.com/api/'}),
+    endpoints: builder => ({
+        createUser: builder.mutation({
+            query: authCredentials => ({
+                url: 'users/create',
+                method: 'POST',
+                body: authCredentials
+            })
+        }),
+        loginUser: builder.mutation({
+            query: authCredentials => ({
+                url: 'users/token',
+                method: 'POST',
+                body: authCredentials
+            })
+        })
+    })
+})
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit"
 import authSlice from "./auth/authSlice"
+import recipesSlice from "./recipes/recipeSlice"
+import { apiSlice } from "../services/DataServices/api"
 
 export const store = configureStore({
     reducer: {
-        auth: authSlice.reducer
-    }
+        auth: authSlice.reducer,
+        recipes: recipesSlice.reducer,
+        [apiSlice.reducerPath]: apiSlice.reducer
+    },
+    middleware: getDefaultMiddleware => 
+        getDefaultMiddleware().concat(apiSlice.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>
