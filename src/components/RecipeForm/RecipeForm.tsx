af44ea76f3c94b4bf136dace069ff75a303ea427
Iulia <iulia.puiu@hotmail.com>
2023-03-03 10:11:15 +0200

    Extract recipe form component

---
+++ src/components/RecipeForm/RecipeForm.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { View, ScrollView } from 'react-native'
+import { TextInput, Button } from 'react-native-paper'
+import { MyImagePicker } from "../ImagePicker/ImagePicker"
+import { useStyles } from "./styles"
+import { useTheme } from 'react-native-paper'
+
+interface IRecipeForm {
+    title: string
+    setTitle: (title: string) => void
+    description?: string
+    setDescription?: (description: string) => void
+    link?: string
+    setLink?: (link: string) => void
+    setImageUri: (imageUri: string) => void
+    onSave: () => void
+}
+
+export const RecipeForm = ({
+        title, 
+        setTitle, 
+        description,
+        setDescription,
+        link, 
+        setLink, 
+        setImageUri, 
+        onSave
+    }: IRecipeForm) => {
+
+    const theme = useTheme()
+    const styles = useStyles(theme)
+
+    return (
+        <View style={styles.mainView} >
+            <ScrollView>
+                <TextInput 
+                    label={"Title"}
+                    mode="outlined"
+                    value={title}
+                    onChangeText={setTitle}
+                    style={styles.input}
+                />
+                <TextInput 
+                    label={"Link (optional)"}
+                    mode="outlined"
+                    value={link}
+                    onChangeText={setLink}
+                    style={styles.input}
+                />
+                <TextInput 
+                    label={"Description (optional)"}
+                    mode="outlined"
+                    value={description}
+                    multiline={true}
+                    onChangeText={setDescription}
+                    style={styles.inputDescirption}
+            />
+                <MyImagePicker 
+                    onSubmit={(file: string) => {setImageUri(file)}}
+            />
+            </ScrollView>
+            <View>
+                <Button 
+                    onPress={onSave}
+                    mode='contained'
+                    style={{marginBottom: 4}}
+                >{'Save'}</Button>
+            </View>
+        </View>
+    )
+}
--- src/screens/Recipes/AddRecipe/AddRecipeScreen.tsx
+++ src/screens/Recipes/AddRecipe/AddRecipeScreen.tsx
@@ -8,6 +8,8 @@ import { colors } from '../../../theme/generalColors'
 import Icon from 'react-native-vector-icons/Ionicons'
 import { TextInput, Button } from 'react-native-paper'
 import { MyImagePicker } from '../../../components/ImagePicker/ImagePicker'
+import { NavBarIcon } from '../../../components/NavBarIcon/NavBarIcon'
+import { RecipeForm } from '../../../components/RecipeForm/RecipeForm'
 
 const AddRecipeScreen = () => {
     const theme = useTheme()
@@ -58,40 +60,6 @@ const AddRecipeScreen = () => {
     //TODO: Create separate components for each of the steps to create a recipe
     //TODO: Add skip button
 
-   
-
-    const DetailsView = () => {
-        return (
-            <>
-                <TextInput 
-                    label={"Duration (optional)"}
-                    mode="outlined"
-                    value={duration}
-                    onChangeText={setDuration}
-                    //keyboardType="email-address"
-                    style={styles.input}
-                />
-                <TextInput 
-                    label={"Link (optional)"}
-                    mode="outlined"
-                    value={link}
-                    onChangeText={setLink}
-                    //keyboardType="email-address"
-                    style={styles.input}
-                />
-                <TextInput 
-                    label={"Description (optional)"}
-                    mode="outlined"
-                    value={description}
-                    multiline={true}
-                    onChangeText={setDescription}
-                    //keyboardType="email-address"
-                    style={styles.inputDescirption}
-                />
-            </>
-        )
-    }
-
     const ListView= (list: [], listTitle: string, setItem: any, placeholder: string, label: string, value: string, renderListItem: any) => {
         return (
             <>
@@ -119,53 +87,43 @@ const AddRecipeScreen = () => {
         )
     }
 
-    const FormView = () => {
-        switch(index) {
-            case 0:
-                return <MainData  value={title} setValue={setTitle} />
-            case 1:
-                return <ListView list={ingredients} listTitle={'Ingredients'} setItem={setIngredient} placeholder={'Add Ingredient, eg 200g meat, 2 eggs (optional)'} label={'Ingredient'} value={ingredient} />
-            case 2:
-                return <ListView  list={steps} listTitle={'Steps'} setItem={setStep} placeholder={'Add Step, eg Scramble eggs for 1 min (optional)'} label={'Step'} value={step}/>
-            case 3:
-                return <ListView list={tags} listTitle={'Tags'} setItem={setTag} placeholder={'Add Tag, eg Vegetarian, Keto'} label={'Tag'} value={tag}/>
-            case 4:
-                return <DetailsView />
-            default: 
-                return null
-        }
-    }
+    // const FormView = () => {
+    //     switch(index) {
+    //         case 0:
+    //             return <MainData  value={title} setValue={setTitle} />
+    //         case 1:
+    //             return <ListView list={ingredients} listTitle={'Ingredients'} setItem={setIngredient} placeholder={'Add Ingredient, eg 200g meat, 2 eggs (optional)'} label={'Ingredient'} value={ingredient} />
+    //         case 2:
+    //             return <ListView  list={steps} listTitle={'Steps'} setItem={setStep} placeholder={'Add Step, eg Scramble eggs for 1 min (optional)'} label={'Step'} value={step}/>
+    //         case 3:
+    //             return <ListView list={tags} listTitle={'Tags'} setItem={setTag} placeholder={'Add Tag, eg Vegetarian, Keto'} label={'Tag'} value={tag}/>
+    //         case 4:
+    //             return <DetailsView />
+    //         default: 
+    //             return null
+    //     }
+    // }
 
     return (
         <Screen
             navBarTitle='Add New Recipe'
             backgroundColor={colors.white}
             navBarBackgroundColor={theme.colors.primary}
-            backArrowButton={true}
-            leftButtonAction={goBack}
-        >
-            <View style={styles.mainView} >
-                <View>
-                    <TextInput 
-                        label={"Title"}
-                        mode="outlined"
-                        value={title}
-                        onChangeText={setTitle}
-                        keyboardType="email-address"
-                        style={styles.input}
-                    />
-                    <MyImagePicker 
-                        onSubmit={(file: string) => {setImageUri(file)}}
+            leftButton={<NavBarIcon 
+                onPress={goBack}
+                name='arrow-back'
+            />}
+        >   
+                <RecipeForm 
+                    title={title}
+                    setTitle={setTitle}
+                    description={description}
+                    setDescription={setDescription}
+                    link={link}
+                    setLink={setLink}
+                    setImageUri={setImageUri}
+                    onSave={onSave}
                 />
-                </View>
-                <View>
-                    <Button 
-                        onPress={onSave}
-                        mode='contained'
-                        style={{marginBottom: 24}}
-                    >{'Save'}</Button>
-                </View>
-            </View>
         </Screen>
     )
 }
