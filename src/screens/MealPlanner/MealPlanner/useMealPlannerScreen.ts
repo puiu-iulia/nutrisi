35e3653261594ee4602cc2a67a928e0eeec74c80
Iulia <iulia.puiu@hotmail.com>
2023-03-07 16:51:09 +0200

    Filter daily recipes

--- src/screens/MealPlanner/MealPlanner/useMealPlannerScreen.ts
+++ src/screens/MealPlanner/MealPlanner/useMealPlannerScreen.ts
@@ -3,11 +3,14 @@ import { useNavigation } from '../../../navigation/useNavigation'
 import routes from '../../../navigation/routes'
 import { useGetMealPlansQuery } from '../../../store/apiSlice'
 import moment from 'moment'
+import { useStyles } from './styles'
 
 export const useMealPlannerScreen = () => {
     const { navigate } = useNavigation()
+    const styles = useStyles()
 
     const [selectedDate, setSelectedDate] = useState(moment.now())
+    const [dailyMeals, setDailyMeals] = useState([])
 
     const {data = [], isLoading, error} = useGetMealPlansQuery()
 
@@ -17,9 +20,40 @@ export const useMealPlannerScreen = () => {
         })
     }
 
+    const onMealPress = (mealId: number) => {
+        if (mealId !== 0) {
+            navigate(routes.RecipeDetails, {
+                recipeId: mealId
+            })
+        } else {
+            onAddMeal()
+        }
+    }
+
+    useEffect(() => {
+        if (data) {
+            filterMeals(selectedDate)
+        }
+    }, [data])
+
+    const filterMeals = (date: any) => {
+        data.filter((meal: any) => {
+            if (meal.date == moment(date).format('YYYY-MM-DD')) {
+                console.log('meal', meal.date)
+                setDailyMeals(meal.recipes.concat([{id: 0, title: 'Add Meal'}])) 
+            } else {
+                setDailyMeals([])
+            }
+        })
+    }
+
     return {
         meals: data,
+        dailyMeals,
         onAddMeal, 
-        setSelectedDate
+        setSelectedDate,
+        styles,
+        filterMeals,
+        onMealPress
     }
 }
